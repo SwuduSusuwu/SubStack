2644adddc323800eced930222fd543487d3f739d
SwuduSusuwu <2002luvabbaluvu@gmail.com>
2024-05-06 18:36:23 -0700

    @c++/ClassCns.h ApxrCns subclass of Cns, just
    prototype (work-in-progress,) don't know howto use Erlang from C++.
    
    Add URLs (resources to assist implementations of `HsomCns` + `ApxrCns`)
    such as https://github.com/Rober-t/apxr_run

--- c++/ClassCns.h
+++ c++/ClassCns.h
@@ -24,12 +24,29 @@ typedef class Cns {
   virtual const Output process(Input input);
 } Cns;
 
-#ifdef USE_HSOM /* Todo. ( https://stackoverflow.com/questions/3286448/calling-a-python-method-from-c-c-and-extracting-its-return-value ) suggests various syntaxes to use for this, with unanswered comments such as "Does this support classes?" */
-typedef class HsomCns : Cns { /* https://github.com/CarsonScott/HSOM */
+#ifdef USE_HSOM
+typedef class HsomCns : Cns {
+/* Todo: `HSOM` is simple Python-based CNS from https://github.com/CarsonScott/HSOM 
+ * Examples of howto setup `HSOM` as artificial CNS; https://github.com/CarsonScott/HSOM/tree/master/examples
+ * [ https://stackoverflow.com/questions/3286448/calling-a-python-method-from-c-c-and-extracting-its-return-value ] suggests various syntaxes to use for this, with unanswered comments such as "Does this support classes?"
+ */
  HsomCns();
  ~HsomCns();
 } HsomCns;
-#endif /* Todo */
+#endif /* USE_HSOM */
+
+#ifdef USE_APXR
+typedef class ApxrCns : Cns {
+/* Todo: `apxr` is complex Erlang-based CNS from https://github.com/Rober-t/apxr_run/
+ * Examples of howto setup `apxr` as artificial CNS; https://github.com/Rober-t/apxr_run/blob/master/src/examples/
+ * "apxr_run" has various FLOSS neural network activation functions (absolute, average, standard deviation, sqrt, sin, tanh, log, sigmoid, cos), plus sensor functions (vector difference, quadratic, multiquadric, saturation [+D-zone], gaussian, cartesian/planar/polar distances): https://github.com/Rober-t/apxr_run/blob/master/src/lib/functions.erl
+ * Various FLOSS neuroplastic functions (self-modulation, Hebbian function, Oja's function): https://github.com/Rober-t/apxr_run/blob/master/src/lib/plasticity.erl
+ * Various FLOSS neural network input aggregator functions (dot products, product of differences, mult products): https://github.com/Rober-t/apxr_run/blob/master/src/agent_mgr/signal_aggregator.erl
+ * Various simulated-annealing functions for artificial neural networks (dynamic [+ random], active [+ random], current [+ random], all [+ random]): https://github.com/Rober-t/apxr_run/blob/master/src/lib/tuning_selection.erl
+ * Choices to evolve connections through Darwinian or Lamarkian formulas: https://github.com/Rober-t/apxr_run/blob/master/src/agent_mgr/neuron.erl
+ */
+} ApxrCns;
+#endif /* USE_APRXR */
 
 /* Possible uses of artificial CNS:
  * Virus analysis; https://swudususuwu.substack.com/p/howto-produce-better-virus-scanners
