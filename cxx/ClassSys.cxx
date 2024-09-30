SwuduSusuwu/SubStack

commit 1ca8d4df335973851d8df1386741d76b9352da6d

?cxx/ClassSys.*xx:+classSysInit,+classSysArg{c, s}
?cxx/main.cxx:?main /* Store {argc, args} through classSysInit; now other functions can process as {classSysArgc, classSysArgs} */

?posts/VirusAnalysis.md /* Include this */

cxx/ClassSys.cxx
@@ -17,6 +17,15 @@
 typedef int pid_t;
 #endif /* def _POSIX_VERSION */
 namespace Susuwu {
+int classSysArgc = 0;
+const char **classSysArgs = {nullptr};
+void classSysInit(int argc, const char *args[]) {
+	if(0 < (classSysArgc = argc)) {
+		assert(nullptr != (classSysArgs = args));
+		assert(nullptr != args[0]);
+	}
+}
+
 const pid_t execvesFork(const std::vector<const std::string> &argvS, const std::vector<const std::string> &envpS) {
 #ifdef _POSIX_VERSION
 	const pid_t pid = fork();

cxx/ClassSys.hxx
@@ -13,6 +13,14 @@ typedef int pid_t;
 #include <vector> /* std::vector */
 /* Abstractions to do with: `sh` scripts (such as: exec*, sudo), sockets (TODO), filesystems (TODO) */
 namespace Susuwu {
+extern int classSysArgc;
+extern const char **classSysArgs;
+/* Called from main(), stores {argc, args} into {classSysArgc, classSysArgs}
+ * Much simpler to use path from args[0] (versus https://stackoverflow.com/questions/1528298/get-path-of-executable/34109000#34109000)
+ * @pre @code (0 < argc && nullptr != args && nullptr != args[0]
+ * @post @code (0 < classSysArgc && nullptr != classSysArgs && nullptr != classSysArgs[0] */
+void classSysInit(int argc, const char *args[]);
+
 /* `argv = argvS + NULL; envp = envpS + NULL: pid_t pid = fork() || (envpS.empty() ? execv(argv[0], &argv[0]) : execve(argv[0], &argv[0], &envp[0])); return pid;`
  * @pre @code (-1 != access(argv[0], X_OK) @endcode */
 const pid_t execvesFork(/* const std::string &pathname, -- `execve` requires `&pathname == &argv[0]` */ const std::vector<const std::string> &argvS = {}, const std::vector<const std::string> &envpS = {});

cxx/main.cxx
@@ -37,6 +37,7 @@ int testHarnesses() EXPECTS(true) ENSURES(true) {
 }
 }; /* namespace Susuwu */
 int main(int argc, const char **args) {
+	Susuwu::classSysInit(argc, args);
 	return Susuwu::testHarnesses();
 }
 #endif /* ndef INCLUDES_cxx_main_cxx */

commit 5ac753858ab58a624eff9d6c0a9362a57c554429

?classSysInit `classSysArgs=args` out of assert(),
  since `NDEBUG` removes `assert()` (introduced with
  1ca8d4df335973851d8df1386741d76b9352da6d 's +classSysInit());
  segfault (introduced with efe1d2c0df5610800464734f5507ac57e1051340
  which read from sysClassArgs) fixed for `./build --release && ./a.out`
  `NDEBUG` is not defined with `./build && ./a.out`, thus segfault was
  missed.
  ?cxx/ClassSys.cxx /* This */

?README,md /* Warn not to perform tasks within `assert()` */

?cxx/ClassSys.*xx:classSysInit() /* return success bool */
  ?cxx/main.cxx:main() /* test returned bool */

?posts/VirusAnalysis.md /* Include this */

cxx/ClassSys.cxx
@@ -20,11 +20,14 @@ typedef int pid_t;
 namespace Susuwu {
 int classSysArgc = 0;
 const char **classSysArgs = {nullptr};
-void classSysInit(int argc, const char *args[]) {
+bool classSysInit(int argc, const char *args[]) {
 	if(0 < (classSysArgc = argc)) {
-		assert(nullptr != (classSysArgs = args));
+		classSysArgs = args;
+		assert(nullptr != args);
 		assert(nullptr != args[0]);
+		return true;
 	}
+	return false;
 }
 
 const pid_t execvesFork(const std::vector<const std::string> &argvS, const std::vector<const std::string> &envpS) {

cxx/ClassSys.hxx
@@ -20,7 +20,7 @@ extern const char **classSysArgs;
  * Much simpler to use path from args[0] (versus https://stackoverflow.com/questions/1528298/get-path-of-executable/34109000#34109000)
  * @pre @code (0 < argc && nullptr != args && nullptr != args[0]
  * @post @code (0 < classSysArgc && nullptr != classSysArgs && nullptr != classSysArgs[0] */
-void classSysInit(int argc, const char *args[]);
+bool classSysInit(int argc, const char *args[]);
 
 /* `argv = argvS + NULL; envp = envpS + NULL: pid_t pid = fork() || (envpS.empty() ? execv(argv[0], &argv[0]) : execve(argv[0], &argv[0], &envp[0])); return pid;`
  * @pre @code (-1 != access(argv[0], X_OK) @endcode */

cxx/main.cxx
@@ -37,7 +37,8 @@ int testHarnesses() EXPECTS(true) ENSURES(true) {
 }
 }; /* namespace Susuwu */
 int main(int argc, const char **args) {
-	Susuwu::classSysInit(argc, args);
+	const bool classSysInitSuccess = Susuwu::classSysInit(argc, args);
+	assert(classSysInitSuccess);
 	return Susuwu::testHarnesses();
 }
 #endif /* ndef INCLUDES_cxx_main_cxx */