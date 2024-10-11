87270381468265788bcbd0fd1fc9f7252c6174be
SwuduSusuwu <2002luvabbaluvu@gmail.com>
2024-10-11 14:50:32 -0700

    ?`build.sh`:?`CXX_FLAGS_DEBUG` split in 2
      (split into +`CXX_FLAGS_FSAN`, which `x86_64-mingw-w64-g++` excludes) /*  "cannot find -lasan: No such file or directory" fix */
      Is followup to e150e069e4eeab87f608e9233ce6fbe8ca584509
      `x86_64-mingw-w64-g++` now is success with `./build --mingw`

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ sSRC="./cxx/"
 #export CXX_FLAGS="-I${INCLUDES}"
 CXX_FLAGS_ANALYSIS="-Wall -Wno-unused -Wno-unused-function -Wextra -Wno-unused-parameter -Wno-ignored-qualifiers" #TODO: `-Werror` without `-Wno-*`
 CXX_FLAGS_RELEASE="${CXX_FLAGS_ANALYSIS} -fomit-frame-pointer -DNDEBUG -O2"
-CXX_FLAGS_DEBUG="${CXX_FLAGS_ANALYSIS} -fsanitize=address -fno-sanitize-recover=all -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize=null -fno-sanitize=alignment" #/* supports `g++`/`clang++`: https://developers.redhat.com/blog/2021/05/05/memory-error-checking-in-c-and-c-comparing-sanitizers-and-valgrind#tldr */
-CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} -fno-omit-frame-pointer" #/* thus optimization won't remove stacktraces: https://stackoverflow.com/questions/48234575/g-will-fno-omit-frame-pointer-be-effective-if-specified-before-o2-or-o3 https://clang.llvm.org/docs/MemorySanitizer.html */
+CXX_FLAGS_FSAN="-fsanitize=address -fno-sanitize-recover=all -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize=null -fno-sanitize=alignment"
+#CXX_FLAGS_FSAN="${CXX_FLAGS_FSAN} -fsanitize=undefined" #/* causes 'cannot locate symbol "__ubsan_handle_function_type_mismatch_abort"' */
+CXX_FLAGS_DEBUG="${CXX_FLAGS_ANALYSIS} -fno-omit-frame-pointer" #/* thus optimization won't remove stacktraces: https://stackoverflow.com/questions/48234575/g-will-fno-omit-frame-pointer-be-effective-if-specified-before-o2-or-o3 https://clang.llvm.org/docs/MemorySanitizer.html */
 CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} -g" #/* extra symbols (line numbers + arguments) to stacktraces */
 #CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} -fno-optimize-sibling-calls" #/* Don't inline functions. Does extra stacktraces. */
-#CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} -fsanitize=undefined" #/* causes 'cannot locate symbol "__ubsan_handle_function_type_mismatch_abort"' */
 if command -v ctags > /dev/null; then
 	ctags -R
 fi
@@ -20,16 +20,20 @@ if [ "--mingw" = "$1" ] || [ "--mingw" = "$2" ]; then
 	CROSS_COMP=" --mingw"
 	if command -v x86_64-w64-mingw32-clang++ > /dev/null; then
 		CXX="x86_64-w64-mingw32-clang++"
+		CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} ${CXX_FLAGS_FSAN}" #/* Supports `x86_64-w64-mingw32-clang++`: https://github.com/SwuduSusuwu/SubStack/issues/16 */
 	elif command -v x86_64-w64-mingw32-g++ > /dev/null; then
 		CXX="x86_64-w64-mingw32-g++"
+#		CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} ${CXX_FLAGS_FSAN}" #/* TODO: support `x86_64-w64-mingw32-g++` ( https://www.mingw-w64.org/contribute/#thorough-status-report-for-sanitizers-asan-tsan-usan ) */
 	else
 		SUSUWU_PRINT "${SUSUWU_SH_ERROR}" "\`x86_64-w64-mingw32-clang++ not found\`, \`x86_64-w64-mingw32-g++ not found\`. Do \`apt install llvm-mingw-w64\` or \`apt install mingw-w64\`"
 		exit 1
 	fi
 elif command -v clang++ > /dev/null; then
 	CXX="clang++"
+	CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} ${CXX_FLAGS_FSAN}" #/* Supports `g++`/`clang++`: https://developers.redhat.com/blog/2021/05/05/memory-error-checking-in-c-and-c-comparing-sanitizers-and-valgrind#tldr */
 elif command -v g++ > /dev/null; then
 	CXX="g++"
+	CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG} ${CXX_FLAGS_FSAN}"
 else
 	SUSUWU_PRINT "${SUSUWU_SH_ERROR}" "\`clang++ not found\`, \`g++ not found\`. Do \`apt install clang\` or \`apt install gcc\`"
 	exit 1
